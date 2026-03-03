3f6d9397ea4cd830ac0cb83027270511948a10ec
senamorsin <senamorsin@yandex.ru>
2026-03-03 16:09:01 +0300

    бейзлайн с катубстом

--- fit.py
+++ fit.py
@@ -1,17 +1,21 @@
 from catboost import CatBoostClassifier
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import f1_score
 
 data = pd.read_csv('train.tsv', sep='\t')
+data = data.fillna('')
 X_train, X_test, y_train, y_test = train_test_split(data.drop(['category_id', 'department_id'], axis=1),
-                                                    data[['category_id', 'department_id']],
+                                                    data['department_id'],
                                                     test_size=0.2, random_state=42)
 
-departments = data['department_id'].value_counts()
-categories = data['category_id'].value_counts()
-print(departments)
-print(categories)
+catboost_clf = CatBoostClassifier(iterations=2500, learning_rate=0.01, depth=6, verbose=True, task_type="GPU")
+catboost_clf.fit(X_train,
+                 y_train,
+                 text_features=['title', 'description'],
+                 cat_features=['vendor_name', 'vendor_code', 'shop_category_name'],
+                 use_best_model=True,
+                 eval_set=[(X_test, y_test)])
 
-
-def create_features_from_text(text):
-    features = dict()
+predictions = catboost_clf.predict(X_test)
+print(f1_score(y_test, predictions, average='macro'))
