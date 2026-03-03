35bf1ffd0d3d5932d4fe9e56752086e250832bae
arina <arina.croh@gmail.com>
2026-03-03 21:50:15 +0200

    1

--- fit.py
+++ fit.py
@@ -1,11 +1,15 @@
 from catboost import CatBoostClassifier
 import pandas as pd
 import re
-
+import nltk
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import f1_score
+from sklearn.feature_extraction.text import TfidfVectorizer
 
-
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+nltk.download('punkt_tab')
+nltk.download('stopwords')
 
 #ЗАГРУЗКА ДАТАСЕТА
 data = pd.read_csv('train.tsv', sep='\t')
@@ -15,14 +19,12 @@ data = data.fillna('')
 stop_words = {'<br>', '<br/>', '<p>', '</p>', '<ul>', '</ul>', '<li>', '</li>', '<a>', '</a>', '<b>', '</b>',
     '&quot;', '&nbsp;', '\\r', '\\n', '\\t', '\\xa0', '\n', '\t', ' ', '  ', '   ',}
 
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
 
 
 def clean_text(text):
     text = text.lower()
+    text = re.sub(r'[^а-яёa-z\s]', '', text)
     text = re.sub(r'<[^>]*>', '', text)
-    text = re.sub('\s+', ' ', text)
     text = text.strip()
     tokens = word_tokenize(text)
     stop_words_lmtk = set(stopwords.words('russian'))
@@ -31,14 +33,24 @@ def clean_text(text):
     return f" ".join(filtered_words)
 
 
-
-
-
-
 data['description'] = data['description'].apply(clean_text)
 
 data['vendor_name'] = data['vendor_name'].replace([",Без бренда", "Нет бренда"], None)
 
+vectorizer = TfidfVectorizer()
+vectorizer.fit(data['description'])
+feature_names = vectorizer.get_feature_names_out()
+idf_scores = vectorizer.idf_
+words_idf = pd.DataFrame({'word': feature_names, 'idf': idf_scores})
+words_idf = words_idf.sort_values(by='idf', ascending=True)
+corpus_stopwords = words_idf.head(100)['word'].tolist()
+print(corpus_stopwords[:20]) #top 20 stopwords
+with open('custom_stopwords.txt', 'w', encoding='utf-8') as f:
+    for word in corpus_stopwords:
+        f.write(f"{word}\n")
+
+
+#SPLIT
 X_train, X_test, y_train, y_test = train_test_split(data.drop(['category_id', 'department_id'], axis=1),
                                                     data['department_id'],
                                                     test_size=0.2, random_state=42)
@@ -53,5 +65,8 @@ catboost_clf.fit(X_train,
 
 predictions = catboost_clf.predict(X_test)
 print(f1_score(y_test, predictions, average='macro'))
+
+catboost_clf.save_model("model.json", format="json")
+
 
 
